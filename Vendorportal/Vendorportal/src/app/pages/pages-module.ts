915d2dda29a97677036369c9df11e79a0daf927e
BIJAY KUMAR MURMU <edubijaykumarmurmu@gmail.com>
2025-09-07 12:17:32 +0530

    update

--- Vendorportal/Vendorportal/src/app/pages/pages-module.ts
+++ Vendorportal/Vendorportal/src/app/pages/pages-module.ts
@@ -7,7 +7,7 @@ import { MainHeader } from "../core/header/main/main-header/main-header";
 
 @Component({
   selector: 'app-pages-module',
-  imports: [RouterOutlet, Header, Footer, MainHeader],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './pages-module.html',
   styleUrl: './pages-module.css'
 })
